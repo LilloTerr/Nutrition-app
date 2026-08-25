2ad18c0bc4360fc19c1150b726a903facc9aaa5f
LilloTerr <lilloterr@libero.it>
2026-08-25 13:32:53 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,34 @@
-const CACHE_NAME = 'nutrition-daily-performance-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './background.png',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME='nutrition-daily-performance-v2';
+const ASSETS=[
+ './',
+ './index.html',
+ './manifest.webmanifest',
+ './background.png',
+ './icon-192.png',
+ './icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+ self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
-  self.clients.claim();
+self.addEventListener('activate',event=>{
+ event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+ );
+ self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET') return;
+ event.respondWith(
+  fetch(event.request).then(response=>{
+   const copy=response.clone();
+   if(event.request.url.startsWith(self.location.origin)){
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+   }
+   return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+ );
 });
